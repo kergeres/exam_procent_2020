13b1fb4e17b6083d04d35aeb66f381ca371134f2
David <eaadpa@students.eaaa.dk>
2020-05-27 23:33:51 +0200

    27_05/5

--- script/script.js
+++ script/script.js
@@ -11,17 +11,13 @@ function kitar() {
         lineOne.classList.toggle("csik1_a");
         lineTwo.classList.toggle("csik2_a");
         lineThree.classList.toggle("csik3_a");
-
     });
-
 }
-
 //navigation bar - animation
 $(window).on('scroll', function () {
     if ($(window).scrollTop()) {
         $('nav').addClass('navbar_active');
         $('.cimer').addClass('cimer_small');
-
 
     } else {
         $('nav').removeClass('navbar_active');
@@ -29,16 +25,12 @@ $(window).on('scroll', function () {
     }
 })
 
-
-
 //text appear animation
 
 function textAnomatoin() {
     var p1 = document.querySelector(".show1");
     var introPosition = p1.getBoundingClientRect().top;
-
     var screenPosition = window.innerHeight / 1.3;
-
     if (introPosition < screenPosition) {
         p1.classList.add("show_on1");
     }
@@ -47,9 +39,7 @@ function textAnomatoin() {
 function textAnomatoin2() {
     var p2 = document.querySelector(".show2");
     var introPosition2 = p2.getBoundingClientRect().top;
-
     var screenPosition = window.innerHeight / 1.3;
-
     if (introPosition2 < screenPosition) {
         p2.classList.add("show_on2");
     }
@@ -58,47 +48,28 @@ function textAnomatoin2() {
 function textAnomatoin3() {
     var p3 = document.querySelector(".show3");
     var introPosition3 = p3.getBoundingClientRect().top;
-
     var screenPosition = window.innerHeight / 1.3;
-
     if (introPosition3 < screenPosition) {
         p3.classList.add("show_on3");
     }
 }
 
-
-
 window.addEventListener('scroll', textAnomatoin);
 window.addEventListener('scroll', textAnomatoin2);
 window.addEventListener('scroll', textAnomatoin3);
 
-
-
 kitar();
-
-
-
 
 //mapbok
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2VyZ2VyZXMiLCJhIjoiY2s3ZDE2OWptMGNlcDNucHE0dTJzaXRubyJ9.n8mW2tGizEW9Hwvu26iG9g';
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/kergeres/ckaobssla6l3y1io01cc86o69',
+    container: 'map',
+    style: 'mapbox://styles/kergeres/ckaobssla6l3y1io01cc86o69',
     center: [10.128532, 56.156348],
-zoom: 12
-
-
+    zoom: 12
 });
 
 var marker = new mapboxgl.Marker()
-.setLngLat([10.128532, 56.156348])
-.addTo(map);
-
-
-
-
-
-
-
-
+    .setLngLat([10.128532, 56.156348])
+    .addTo(map);
