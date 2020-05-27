2426dee9b2b6d8b25bd6b361d637f024622bc60a
David <eaadpa@students.eaaa.dk>
2020-05-27 10:24:39 +0200

    27_05

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// burger menu navigation
 function kitar() {
     const burger = document.querySelector("#burger_menu");
     const nav = document.querySelector(".nav_links");
@@ -15,7 +16,7 @@ function kitar() {
 
 }
 
-
+//navigation bar - animation
 $(window).on('scroll', function () {
     if ($(window).scrollTop()) {
         $('nav').addClass('navbar_active');
@@ -28,41 +29,41 @@ $(window).on('scroll', function () {
     }
 })
 
-function textAnomatoin ()
-{
+
+
+//text appear animation
+
+function textAnomatoin() {
     var p1 = document.querySelector(".show1");
     var introPosition = p1.getBoundingClientRect().top;
 
-    var screenPosition = window.innerHeight/1.3;
+    var screenPosition = window.innerHeight / 1.3;
 
-    if(introPosition < screenPosition)
-        {
-            p1.classList.add("show_on1");
-        }
+    if (introPosition < screenPosition) {
+        p1.classList.add("show_on1");
+    }
 }
- function textAnomatoin2 ()
-{
+
+function textAnomatoin2() {
     var p2 = document.querySelector(".show2");
     var introPosition2 = p2.getBoundingClientRect().top;
 
-    var screenPosition = window.innerHeight/1.3;
+    var screenPosition = window.innerHeight / 1.3;
 
-    if(introPosition2 < screenPosition)
-        {
-            p2.classList.add("show_on2");
-        }
+    if (introPosition2 < screenPosition) {
+        p2.classList.add("show_on2");
+    }
 }
-function textAnomatoin3 ()
-{
+
+function textAnomatoin3() {
     var p3 = document.querySelector(".show3");
     var introPosition3 = p3.getBoundingClientRect().top;
 
-    var screenPosition = window.innerHeight/1.3;
+    var screenPosition = window.innerHeight / 1.3;
 
-    if(introPosition3 < screenPosition)
-        {
-            p3.classList.add("show_on3");
-        }
+    if (introPosition3 < screenPosition) {
+        p3.classList.add("show_on3");
+    }
 }
 
 
@@ -71,7 +72,30 @@ window.addEventListener('scroll', textAnomatoin);
 window.addEventListener('scroll', textAnomatoin2);
 window.addEventListener('scroll', textAnomatoin3);
 
+
+
 kitar();
+
+
+
+
+//mapbok
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2VyZ2VyZXMiLCJhIjoiY2s3ZDE2OWptMGNlcDNucHE0dTJzaXRubyJ9.n8mW2tGizEW9Hwvu26iG9g';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/kergeres/ckaobssla6l3y1io01cc86o69',
+    center: [10.128532, 56.156348],
+zoom: 12
+
+
+});
+
+var marker = new mapboxgl.Marker()
+.setLngLat([10.128532, 56.156348])
+.addTo(map);
+
+
 
 
 
